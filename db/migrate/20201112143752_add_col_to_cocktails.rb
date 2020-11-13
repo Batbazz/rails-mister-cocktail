7d134590ce63267a1c9ef8128fdb6978afb7c4d0
Basile Rieu <basilerieu@yahoo.fr>
2020-11-13 12:40:42 +0100

    Add dotenv - Protect my secret data in .env file

---
+++ db/migrate/20201112143752_add_col_to_cocktails.rb
@@ -0,0 +1,5 @@
+class AddColToCocktails < ActiveRecord::Migration[6.0]
+  def change
+    add_column :cocktails, :description, :text
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# require 'faker'
+require 'open-uri'
+require 'json'
+
+puts 'seeding'
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+# Ingredient.create(name: 'lemon')
+# Ingredient.create(name: 'ice')
+# Ingredient.create(name: 'mint leaves')
+
+# 10.times do
+  # c = Cocktail.new(name: Faker::Coffee.blend_name)
+  # p c
+  # c.save!
+  # puts 'Created one cocktail'
+# end
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+serialized_ingredients = open(url).read
+
+ingredients = JSON.parse(serialized_ingredients)
+
+ingredients['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+
+end
+
+puts 'Done seeding'
